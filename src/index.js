e1232ce3cbde73b1fe29824c71cc66ad5d62e48d
keksik77 <lukashevich7tim@gmail.com>
2020-02-24 23:40:09 +0300

    feat: completed solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array[0] === undefined){
+    return 0;
+  }else{
+    array = array.sort(function (prev, next) {
+      return prev - next;
+    })
+    return array[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array[0] === undefined){
+    return 0;
+  }else{
+    array = array.sort(function (prev, next) {
+      return prev - next;
+    })
+    return array[array.length - 1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array[0] === undefined){
+    return 0;
+  }else{
+    let Avg = 0;
+    array.forEach(element => { Avg += element});
+    return (Avg/array.length);
+  }
 }
